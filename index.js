d6fd723d8d367ef1ea4fcb0d7e35650a7ec93656
FAForhad <faforhad450@gmail.com>
2023-01-04 11:10:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const app = express()
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+require('dotenv').config()
+
+
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.c3txqlb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const sectorCollections = client.db('DataSectors').collection('sectors')
+
+
+
+        app.get('/sectors', async (req, res) => {
+            const query = {}
+            const result = await sectorCollections.find(query).toArray()
+            res.send(result)
+        })
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.log())
+
+app.get('/', (req, res) => {
+    res.send('DataSectors Are Running')
+})
+
+app.listen(port, () => {
+    console.log(`server is running at ${port}`)
+})
